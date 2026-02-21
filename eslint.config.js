53eddafe56f9e36dfc0215a3c8420d5f7f41fb0d
Mike Hall <mike@just3ws.com>
2026-02-21 08:04:20 -0600

    chore: stabilize ESLint and finalize documentation artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ export default tseslint.config(
     ignores: [
       '**/dist/**',
       '**/node_modules/**',
+      'docs/api/**',
       'shared/json-schema/**',
       'shared/src/types.ts',
     ],
